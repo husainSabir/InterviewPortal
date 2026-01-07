ad647bf0cca1246f56bee0e141b62d850bec5d48
husainSabir <2019267@iiitdmj.ac.in>
2026-01-07 12:26:37 +0530

    Added .env

--- server/config/db.connect.ts
+++ server/config/db.connect.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const initializeDBConnection = async (): Promise<void> => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017");
+    await mongoose.connect( process.env.DATABASE_URL || "mongodb://127.0.0.1:27017" );
     console.log("Connected To Database!");
   } catch (error) {
     console.error("Connection To Database failed.", error);
